9e23cdbac566d52b2f6e9fcdb5aed7bcd8487208
Kongphop Jitsongserm <kongphop.jitsongserm@gmail.com>
2025-03-25 21:57:08 +0700

    edit booking layout

--- src/app/editbooking/layout.tsx
+++ src/app/editbooking/layout.tsx
@@ -4,7 +4,7 @@ import BookingMenu from "@/components/BookingMenu";
 export default function BookingLayout( {children}:{children:React.ReactNode} ){
     return (
         <div >
-            <BookingMenu/>
+
             {children}
         </div>
     );
