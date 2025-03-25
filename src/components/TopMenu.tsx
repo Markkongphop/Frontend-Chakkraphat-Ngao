ae69edad277849fdc89c95a892c705bc88f6d621
Kongphop Jitsongserm <kongphop.jitsongserm@gmail.com>
2025-03-25 20:32:03 +0700

    adjust topmenu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -17,14 +17,14 @@ export default async function TopMenu() {
                             <div className={`${styles.signInButton} flex items-center`}>Sign-Out</div>
                         </Link>
                     ) : (
-                        <div className='mt-10 mx-2 flex flex-col items-start'>
-    <Link href="/register" className='my-2'>
-        <div className={`${styles.blueButton} flex items-center`}>Register</div>
-    </Link>
-    <Link href="/api/auth/signin" className='my-2'>
-        <div className={`${styles.signInButton} flex items-center`}>Sign-In</div>
-    </Link>
-</div>
+                        <div className='  flex items-center'>
+                            <Link href="/register" className='my-2 mx-2'>
+                                <div className={`${styles.blueButton} flex items-center`}>Register</div>
+                            </Link>
+                            <Link href="/api/auth/signin" className='my-2 mx-2'>
+                                <div className={`${styles.signInButton} flex items-center`}>Sign-In</div>
+                            </Link>
+                        </div>
                     )
                 }
 
